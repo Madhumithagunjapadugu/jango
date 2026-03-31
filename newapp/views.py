4269e7708fd593078f2962a8468ea57d9a175d92
Madhumithagunjapadugu <madhumithagunjapadugu@gmail.com>
2026-03-31 14:59:01 +0530

    secondchange

--- newapp/views.py
+++ newapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from newapp.forms import schl_form
+
+def school (request):
+    form=schl_form()
+    if request.method=="POST":
+        form=schl_form(request.POST)
+        if form.is_valid():
+            form.save()
+    context={'form':form}
+    return render(request,'home.html',context)
